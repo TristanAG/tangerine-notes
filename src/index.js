36f8f0425ad903c7f66cd6c1953498863c0cb42e
TristanAG <tristan.gruener@gmail.com>
2018-02-12 17:33:39 -0800

    initial hash grabbin

--- src/index.js
+++ src/index.js
@@ -11,23 +11,53 @@ const Header = () => (
     <div className="container">
       <a href="http://tristangruener.com">tristangruener.com</a>
     </div>
+    <hr />
   </header>
 )
+
+class AddNote extends React.Component {
+  addNote(e) {
+    e.preventDefault()
+    const note = e.target.elements.note.value;
+    console.log(note)
+    e.target.elements.note.value = ''
+  }
+  showKey(e) {
+    let input = e.target.value
+    input = input[input.length -1]
+    console.log('key get', input)
+
+    if(input === '#'){
+      console.log('hashntime!!')
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.addNote}>
+          <textarea name="note" onKeyUp={this.showKey}/>
+          <br />
+          <button type="submit">add</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 class TangerineNotes extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <div className="container">
-          <h4>Tangerine Notes 🍊</h4>
+        <div className="container" style={{ marginTop: "15px"}}>
+          <h4>Tangerine Notes <span role="img" alt="tangerine emoji" aria-label="tangerine emoji">🍊</span></h4>
+          <AddNote />
         </div>
       </div>
     )
   }
 }
-
-const jsx = <h4>Tangerine Notes 🍊</h4>
 
 ReactDOM.render(<TangerineNotes />, document.getElementById('root'));
 registerServiceWorker();
